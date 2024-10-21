1d001df50e8b2afe3f038d123358ca41f63ccff5
sirkaue <kauekdslopes@gmail.com>
2024-10-21 19:45:07 -0300

    Initial commit

---
+++ src/main/java/com/sirkaue/demojunit5/DemoJunit5Application.java
@@ -0,0 +1,13 @@
+package com.sirkaue.demojunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJunit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJunit5Application.class, args);
+    }
+
+}
