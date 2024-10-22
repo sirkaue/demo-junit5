8e91b0970a2861a6476001ad9fc357531999660d
sirkaue <kauekdslopes@gmail.com>
2024-10-22 17:01:20 -0300

    criando exception

---
+++ src/main/java/com/sirkaue/demojunit5/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sirkaue.demojunit5.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
