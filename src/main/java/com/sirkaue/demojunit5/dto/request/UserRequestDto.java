d002793f909d591f92fafa8913d7511c5c9c130e
sirkaue <kauekdslopes@gmail.com>
2024-10-23 16:23:02 -0300

    feat: padrao dto

---
+++ src/main/java/com/sirkaue/demojunit5/dto/request/UserRequestDto.java
@@ -0,0 +1,4 @@
+package com.sirkaue.demojunit5.dto.request;
+
+public record UserRequestDto(String name, String email, String password) {
+}
---
+++ src/main/java/com/sirkaue/demojunit5/dto/response/UserResponseDto.java
@@ -0,0 +1,4 @@
+package com.sirkaue.demojunit5.dto.response;
+
+public record UserResponseDto(Long id, String name, String email) {
+}
