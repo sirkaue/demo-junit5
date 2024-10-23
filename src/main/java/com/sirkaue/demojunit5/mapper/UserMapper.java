955b6fe0a695150d621fdf4e159da8d916a3f9c0
sirkaue <kauekdslopes@gmail.com>
2024-10-23 19:44:22 -0300

    feat(mapper): adicionando método para mapeamento de lista de usuários

--- src/main/java/com/sirkaue/demojunit5/mapper/UserMapper.java
+++ src/main/java/com/sirkaue/demojunit5/mapper/UserMapper.java
@@ -4,6 +4,9 @@ import com.sirkaue.demojunit5.domain.User;
 import com.sirkaue.demojunit5.dto.request.UserRequestDto;
 import com.sirkaue.demojunit5.dto.response.UserResponseDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserMapper {
 
     private UserMapper() {
@@ -19,5 +22,12 @@ public class UserMapper {
 
     public static UserResponseDto toUserDto(User user) {
         return new UserResponseDto(user.getId(), user.getName(), user.getEmail());
+    }
+
+
+    public static List<UserResponseDto> toUserDto(List<User> users) {
+        return users.stream()
+                .map(UserMapper::toUserDto)
+                .collect(Collectors.toList());
     }
 }
