7f01094de4909bfabe4cc5eb904fd7015af96176
sirkaue <kauekdslopes@gmail.com>
2024-10-23 19:45:32 -0300

    feat: adicionando contrato findAll no service

--- src/main/java/com/sirkaue/demojunit5/service/UserService.java
+++ src/main/java/com/sirkaue/demojunit5/service/UserService.java
@@ -2,7 +2,11 @@ package com.sirkaue.demojunit5.service;
 
 import com.sirkaue.demojunit5.dto.response.UserResponseDto;
 
+import java.util.List;
+
 public interface UserService {
 
     UserResponseDto findById(Long id);
+
+    List<UserResponseDto> findAll();
 }
