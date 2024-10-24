b88c4257c763401d8a408f34596c8b102c85ee8a
sirkaue <kauekdslopes@gmail.com>
2024-10-24 19:07:39 -0300

    feat: adiciona novo construtor ao StandardError e novos getters e setters

--- src/main/java/com/sirkaue/demojunit5/exception/StandardError.java
+++ src/main/java/com/sirkaue/demojunit5/exception/StandardError.java
@@ -1,5 +1,8 @@
 package com.sirkaue.demojunit5.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+
 import java.time.LocalDateTime;
 
 public class StandardError {
@@ -9,16 +12,27 @@ public class StandardError {
     private String error;
     private String message;
     private String path;
+    private String method;
 
     public StandardError() {
     }
 
-    public StandardError(LocalDateTime timestamp, int status, String error, String message, String path) {
+    public StandardError(LocalDateTime timestamp, int status, String error, String message, String path, String method) {
         this.timestamp = timestamp;
         this.status = status;
         this.error = error;
         this.message = message;
         this.path = path;
+        this.method = method;
+    }
+
+    public StandardError(HttpServletRequest request, HttpStatus status, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
     }
 
     public LocalDateTime getTimestamp() {
@@ -59,5 +73,13 @@ public class StandardError {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
     }
 }
