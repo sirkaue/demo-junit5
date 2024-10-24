a69e75fd6bcdfde0e6e2dc9d0295ed0b96acd602
sirkaue <kauekdslopes@gmail.com>
2024-10-24 19:19:56 -0300

    feat: adiciona método create no resource e define endpoint id como constante

--- src/main/java/com/sirkaue/demojunit5/resource/UserResource.java
+++ src/main/java/com/sirkaue/demojunit5/resource/UserResource.java
@@ -1,26 +1,28 @@
 package com.sirkaue.demojunit5.resource;
 
+import com.sirkaue.demojunit5.dto.request.UserRequestDto;
 import com.sirkaue.demojunit5.dto.response.UserResponseDto;
 import com.sirkaue.demojunit5.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    public static final String ID = "/{id}";
+    
     private final UserService userService;
 
     public UserResource(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(ID)
     public ResponseEntity<UserResponseDto> findById(@PathVariable Long id) {
         UserResponseDto dto = userService.findById(id);
         return ResponseEntity.ok(dto);
@@ -30,5 +32,15 @@ public class UserResource {
     public ResponseEntity<List<UserResponseDto>> findAll() {
         List<UserResponseDto> users = userService.findAll();
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto dto) {
+        UserResponseDto user = userService.create(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path(ID)
+                .buildAndExpand(user.id())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
