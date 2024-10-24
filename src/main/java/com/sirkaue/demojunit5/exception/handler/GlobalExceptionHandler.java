671f689786ec3487c42895437acd5cb82d798fb2
sirkaue <kauekdslopes@gmail.com>
2024-10-24 19:09:29 -0300

    refactor: refatora GlobalExceptionHandler e adiciona método auxiliar para criação de respostas de erro

--- src/main/java/com/sirkaue/demojunit5/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sirkaue/demojunit5/exception/handler/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.sirkaue.demojunit5.exception.handler;
 
+import com.sirkaue.demojunit5.exception.EmailUniqueViolationException;
 import com.sirkaue.demojunit5.exception.ObjectNotFoundException;
 import com.sirkaue.demojunit5.exception.StandardError;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -17,15 +18,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-        var error = new StandardError(
-                LocalDateTime.now(),
-                NOT_FOUND.value(),
-                "Not Found",
-                e.getMessage(),
-                request.getRequestURI()
-        );
+        return createErrorResponse(request, NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(EmailUniqueViolationException.class)
+    public ResponseEntity<StandardError> emailUniqueViolationException(EmailUniqueViolationException e, HttpServletRequest request) {
+        return createErrorResponse(request, CONFLICT, e.getMessage());
+    }
+
+    private ResponseEntity<StandardError> createErrorResponse(HttpServletRequest request, HttpStatus status, String message) {
+        StandardError error = new StandardError(request, status, message);
         return ResponseEntity
-                .status(NOT_FOUND)
+                .status(status)
                 .contentType(APPLICATION_JSON)
                 .body(error);
     }
