008769cddd78ecf61f60691640c39f24a186f683
sirkaue <kauekdslopes@gmail.com>
2024-10-25 21:08:02 -0300

    feat: adiciona método existsByEmail no repository

--- src/main/java/com/sirkaue/demojunit5/repository/UserRepository.java
+++ src/main/java/com/sirkaue/demojunit5/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.sirkaue.demojunit5.repository;
 
 import com.sirkaue.demojunit5.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE u.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+
 }
