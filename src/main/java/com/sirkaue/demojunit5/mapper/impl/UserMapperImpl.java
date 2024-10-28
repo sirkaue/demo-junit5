ac4d7328f2c5c6556574d2e5f21e7b32b3c88aea
sirkaue <kauekdslopes@gmail.com>
2024-10-28 14:42:16 -0300

    feat: implementa classe concreta do mapper

---
+++ src/main/java/com/sirkaue/demojunit5/mapper/impl/UserMapperImpl.java
@@ -0,0 +1,37 @@
+package com.sirkaue.demojunit5.mapper.impl;
+
+import com.sirkaue.demojunit5.domain.User;
+import com.sirkaue.demojunit5.dto.request.UserRequestDto;
+import com.sirkaue.demojunit5.dto.response.UserResponseDto;
+import com.sirkaue.demojunit5.mapper.UserMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toUser(UserRequestDto dto) {
+        User user = new User();
+        user.setName(dto.name());
+        user.setEmail(dto.email());
+        user.setPassword(dto.password());
+        return user;
+    }
+
+    @Override
+    public UserResponseDto toUserDto(User user) {
+        return new UserResponseDto(user.getId(), user.getName(), user.getEmail());
+    }
+
+    @Override
+    public List<UserResponseDto> toUserDto(List<User> users) {
+        if (users == null) {
+            return List.of();
+        }
+
+        return users.stream()
+                .map(this::toUserDto)
+                .collect(Collectors.toList());
+    }
+}
