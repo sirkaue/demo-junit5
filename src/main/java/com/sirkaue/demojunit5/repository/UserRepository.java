fc15deb17b881e104924555fbc170bc118c4eb45
sirkaue <kauekdslopes@gmail.com>
2024-11-24 19:18:22 -0300

    feat: adiciona método findByEmail

--- src/main/java/com/sirkaue/demojunit5/repository/UserRepository.java
+++ src/main/java/com/sirkaue/demojunit5/repository/UserRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT COUNT(u) > 0 FROM User u WHERE u.email = :email")
     boolean existsByEmail(@Param("email") String email);
 
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
