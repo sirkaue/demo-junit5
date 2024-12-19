2616173afdf5db28e0d0dca8e76c3eaa98e415b3
sirkaue <kauekdslopes@gmail.com>
2024-12-19 20:40:04 -0300

    fix: reinicia contador de IDs e insere dados padrão na tabela de usuários

--- src/test/resources/sql/import.sql
+++ src/test/resources/sql/import.sql
@@ -1,2 +1,4 @@
+ALTER TABLE tb_user ALTER COLUMN id RESTART WITH 1;
+
 insert into tb_user (name, email, password) values ('John Doe', 'john@email.com', '123456');
 insert into tb_user (name, email, password) values ('Kaue', 'kaue@email.com', '123456');
